5799d3fbcd71e59c2d84ea266895b858e88c7369
Martin Correa <martincorrea792@gmail.com>
2023-09-01 13:34:52 -0300

    execute at the same time

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,10 +5,10 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int = 0, max_delay: int = 10) -> float:
+async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """  Basic Async Syntax """
-    tasks: List = []
-    delays: List[float] = []
+    tasks = []
+    delays = []
     for i in range(n):
         tasks.append(wait_random(max_delay))
     for task in asyncio.as_completed(tasks):
