51376d8a302e8080a36cc00bea97e0174466fdbb
Martin Correa <martincorrea792@gmail.com>
2023-09-17 11:20:04 -0300

    fix

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,10 @@
 /* Modify the following function’s budget object to simply use the keyname instead. */
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = 
+  const budget =
   {
-    income, 
-    gdp, 
-    capita
-  };
+    income,
+    gdp,
+    capita,
+  }
   return budget;
 };
